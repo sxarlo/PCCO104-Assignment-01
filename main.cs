05fb0fccc9ad535f4a87bc21ebf548238610345d
sxarlo <158657052+sxarlo@users.noreply.github.com>
2024-02-17 03:46:13 +0000

    final output/added a variable

--- main.cs
+++ main.cs
@@ -4,7 +4,7 @@ class Program {
   public static void Main (string[] args) {
 
     // input value here. example: 0.05m
-    decimal bills = 50;
+    decimal bills = 0.05m;
 
     // the m is to remind the compiler that the number is decimal.
     if (bills == 0.01m || bills == 0.05m || bills ==  0.25m) {
@@ -12,39 +12,39 @@ class Program {
     }
 
     else if (bills == 1){
-      Console.WriteLine("Jose Rizal is found in Php. 1");
+      Console.WriteLine("Jose Rizal is found in Php." + bills);
     }
 
     else if (bills == 5) {
-      Console.WriteLine("Emilio Aguinaldo");
+      Console.WriteLine("Emilio Aguinaldo in Php." + bills);
      }
 
     else if (bills == 10) {
-      Console.WriteLine("Andres Bonifacio & Apolinario Mabini is found in Php. 10");
+      Console.WriteLine("Andres Bonifacio & Apolinario Mabini is found in Php. " + bills);
     }
 
     else if (bills == 20) {
-      Console.WriteLine("Manuel L. Quezon is found in Php. 20");
+      Console.WriteLine("Manuel L. Quezon is found in Php. " + bills);
     }
 
     else if (bills == 50) {
-      Console.WriteLine("Sergio Osmena is found in Php. 50");
+      Console.WriteLine("Sergio Osmena is found in Php. " + bills);
     }
 
     else if (bills == 100) {
-      Console.WriteLine("Manuel Roxas is found in Php. 100");
+      Console.WriteLine("Manuel Roxas is found in Php. " + bills);
     }
 
     else if (bills == 200) {
-       Console.WriteLine("Diosdado Macapagal is found in Php. 200");
+       Console.WriteLine("Diosdado Macapagal is found in Php. " + bills);
     }
 
     else if (bills == 500) {
-       Console.WriteLine("Benigno Sr. & Corazon Aquino is found in Php. 500");
+       Console.WriteLine("Benigno Sr. & Corazon Aquino is found in Php. " + bills);
     }
 
     else if (bills == 1000) {
-       Console.WriteLine("Jose Abad Santos, Vicent Lim, & Josefa Llanes Escoda is found in Php. 1000");
+       Console.WriteLine("Jose Abad Santos, Vicent Lim, & Josefa Llanes Escoda is found in Php. " + bills);
     }
 
     else {
